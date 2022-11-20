cc059f97e0ae9799a6dc421cec5942ed0edf0b5f
user name <user email>
2022-11-20 08:12:52 -0800

    Initial commit

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     username: {
       type: String,
       required: true,
@@ -18,5 +18,11 @@ const userSchema = new mongoose.Schema({
     isAdmin: {
       type: Boolean,
       default: false
-    }
-})
+    },
+},
+{
+  timestamps: true,
+}
+);
+
+module.exports = mongoose.model("User", UserSchema);
