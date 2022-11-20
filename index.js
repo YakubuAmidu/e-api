21672db909e4689c1ed130558990b10c6d4afe14
user name <user email>
2022-11-20 03:11:07 -0800

    Initial commit

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+/*DOTENV-FILE*/
 dotenv.config();
 
+/*DATABASE-CONNECTION*/
 mongoose.connect(process.env.MONGO_URL).then(() => console.log("DB connection successfull...🤗")).catch((err) => console.log(err));
 
+app.get("/api/test", () => {
+  console.log('Test is working...');
+})
+
+/*CONNECTION-PORT*/
 app.listen(process.env.PORT || 5000, () => {
   console.log('Backend server is running😁...')
 });
