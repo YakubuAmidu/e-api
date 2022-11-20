9a220bed902f10cdbea07a838721e5c03333c28f
user name <user email>
2022-11-20 03:03:54 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL).then(() => console.log("DB connection successfull...🤗")).catch((err) => console.log(err));
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log('Backend server is running😁...')
+});
