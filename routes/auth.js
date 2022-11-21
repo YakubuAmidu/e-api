2514cb46ac7ddc1955111057429ed9abb3451b43
user name <user email>
2022-11-20 17:59:39 -0800

    Initial commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
+/*CRIPTOJS*/
+const CryptoJS = require("crypto-js");
+
 const User = require("../models/User");
 
 /*REGISTER*/
 router.post("/register", async (req, res) => {
-  const { username, email, password } = req.body;
+  const { username, email } = req.body;
 
   const newUser = new User({
     username,
     email,
-    password,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SECRET_KEY
+    ).toString(),
   });
 
   try {
     const saveUser = await newUser.save();
-     res.status(200).json(saveUser);
-  } catch(err){
+    res.status(200).json(saveUser);
+  } catch (err) {
     res.status(500).json(err);
-  } 
+  }
 });
 
 module.exports = router;
