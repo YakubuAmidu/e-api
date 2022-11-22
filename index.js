daa234ea4aa369c5e45483cb8d496355fd9a4910
user name <user email>
2022-11-22 06:33:08 -0800

    Initial commit

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
 
 /*DOTENV-FILE*/
 dotenv.config();
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
 
 /*CONNECTION-PORT*/
 app.listen(process.env.PORT || 5000, () => {
--- routes/cart.js
+++ routes/cart.js
@@ -1,5 +1,38 @@
+/*IMPORT-COMPONENTS*/
+const Cart = require("../models/Cart");
+const { verifyToken } = require("./verifyToken");
+
 /*IMPORTED-EXPRESS*/
 const express = require("express");
 const router = express.Router();
+
+/*ADD-CART*/
+router.post("/", verifyToken, async (req, res) => {
+  const newCart = new Cart(req.body);
+
+  try {
+    const savedCart = await newCart.save();
+    res.status(200).json(savedCart);
+    console.log(saveCart);
+  } catch (err) {
+    re.status(500).json(err);
+  }
+});
+
+/*UPDATE*/
+router.put("/:id", verifyToken, async (req, res) => {
+  try {
+    const updatedCart = await Cart.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedCart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
